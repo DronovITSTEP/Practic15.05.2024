2116f217d93fbcdba6e2fe434ae7fdeba1896a87
ApxNord <dronov_a@t.top-academy.ru>
2024-05-15 20:48:28 +0300

    Добавьте файлы проекта.

---
+++ Practic15.05.2024/Practic15.05.2024.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Array.h"
+using namespace std;
+
+int main()
+{
+	Array<int> arr;
+	arr[2] = 8;
+	cout << arr[2];
+	int* num = new int;
+	int* arNum = new int[5] {3, 6, 6};
+	*num = 4;
+	arr.InsertAt(3, arNum);
+}
