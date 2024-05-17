c647d53342360fc8ff4b937191137b9e979e1697
ApxNord <nord-life17@yandex.ru>
2024-05-17 21:12:15 +0300

    доработка методов. Исправление ошибок

--- Practic15.05.2024/Array.h
+++ Practic15.05.2024/Array.h
@@ -45,13 +45,23 @@ class Array
 	int grow;
 	int lastIndex;
 	T* arr;
+
+	void initArr(int size, int for_size) {
+		T* temp = new T[size];
+		for (int i = 0; i < for_size; i++) {
+			temp[i] = arr[i];
+		}
+		delete[] arr;
+		arr = temp;
+	}
 public:
-	Array() :size{ 0 }, arr{ nullptr }, grow{ 1 }, lastIndex{ -1 } {}
+	Array();
 	/// <summary>
 	/// получение размера массива(количество элементов,
 	///	под которые выделена память);
-	/// </summary>	
+	/// </summary>
 	int GetSize();
+
 	/// <summary>
 	/// установка размера массива
 	/// (если параметр size больше предыдущего размера массива, то
@@ -65,7 +75,8 @@ public:
 	/// </summary>
 	/// <param name="size">размер массива</param>
 	/// <param name="grow">размер увеличения массива при его заполнении</param>
-	void SetSize(int size, int grow = 1); 
+	void SetSize(int size, int grow = 1);
+
 	/// <summary>
 	///  получение последнего допустимого индекса в массиве.
 	/// Например, если при размере массива 10, вы добавляете 
@@ -73,25 +84,30 @@ public:
 	/// </summary>
 	/// <returns>последний допустимый индекс в массиве</returns>
 	int GetUpperBound();
+
 	/// <summary>
 	/// массив пуст?
 	/// </summary>
 	/// <returns>true: массив пустой, иначе false</returns>
 	bool IsEmpty();
+
 	/// <summary>
 	/// удалить «лишнюю» память(выше последнего допустимого индекса)
 	/// </summary>
 	void FreeExtra();
+
 	/// <summary>
 	/// удалить все;
 	/// </summary>
 	void RemoveAll();
+
 	/// <summary>
 	/// получение определенного элемента(по индексу)
 	/// </summary>
 	/// <param name="index">индекс</param>
 	/// <returns>элемент по индексу</returns>
 	T& GetAt(int index);
+
 	/// <summary>
 	/// установка нового значения для определенного элемента
 	/// (индекс элемента должен быть меньше текущего размера
@@ -99,43 +115,191 @@ public:
 	/// </summary>
 	/// <param name="index">индекс</param>
 	/// <param name="elem">элемент</param>
-	void SetAt(int index, T elem); 
+	void SetAt(int index, T elem);
+
 	/// <summary>
 	/// для реализации двух предыдущих функций;
 	/// </summary>
 	/// <param name="index">индекс</param>
 	/// <returns>элемент</returns>
 	T& operator [](int index);
+
 	/// <summary>
 	/// добавление элемента в массив(при необходимости
 	/// массив увеличивается на значение grow функции SetSize);
 	/// </summary>
 	/// <param name="elem">эелемент</param>
 	void Add(T elem);
+
 	/// <summary>
 	/// «сложение» двух массивов;
 	/// </summary>
 	/// <param name="arr">массив друго объекта класса</param>
 	Array& Append(const Array& obj);
 	Array& operator=(const Array& obj);
+
 	/// <summary>
 	/// получения адреса массива с данными;
 	/// </summary>
 	/// <returns></returns>
-	T& GetData(); 
+	T* GetData();
+
 	/// <summary>
 	/// вставка элемента(-ов) в заданную позицию;
 	/// </summary>
 	/// <param name="index">индекс</param>
 	/// <param name="elems">элемент(-ы)</param>
-	/// <param name="size">размер</param>
+	/// <param name="size">количество вставляемых элементов. По умолчанию - 1</param>
 	void InsertAt(int index, T* elems, int size = 1);
+
 	/// <summary>
 	/// удаление элемента(-ов) с заданной позиции
 	/// </summary>
 	/// <param name="index">индекс</param>
 	/// <param name="elems">элемент(-ы)</param>
 	/// <param name="size">размер</param>
-	void RemoveAt(int index,  int count = 1); 
+	void RemoveAt(int index, int count = 1);
+
+
+	friend ostream& operator<<(ostream& out, Array& obj) {
+		for (int i = 0; i <= obj.lastIndex; i++) {
+			out << obj.arr[i] << " ";
+		}
+		out << endl;
+		return out;
+	}
+
+	~Array() {
+		if (arr)
+			delete[] arr;
+	}
 };
 
+
+template <class T>
+Array<T>::Array() :size{ 0 }, arr{ nullptr }, grow{ 1 }, lastIndex{ -1 } {}
+
+template <class T>
+int Array<T>::GetSize() {
+	return size;
+}
+
+template <class T>
+void Array<T>::SetSize(int size, int grow) {
+
+	this->grow = grow;
+
+	if (this->size > size) {
+		initArr(size, size);
+	}
+	else if (this->size < size) {
+		initArr(size, this->size);
+	}
+	else {
+		return;
+	}
+	this->size = size;
+}
+
+template <class T>
+int Array<T>::GetUpperBound() {
+	return lastIndex;
+}
+
+template <class T>
+bool Array<T>::IsEmpty() {
+	return lastIndex == -1 ? true : false;
+}
+
+template <class T>
+void Array<T>::FreeExtra() {
+	if (size == lastIndex + 1) return;
+
+	size = lastIndex + 1;
+
+	initArr(size, size);
+}
+
+template <class T>
+void Array<T>::RemoveAll();
+template <class T>
+T& Array<T>::GetAt(int index) {
+	return (index >= 0 && index <= lastIndex) ? arr[index] : arr[0];
+}
+
+template <class T>
+void Array<T>::SetAt(int index, T elem) {
+	if (index >= 0 && index < size)
+		arr[index] = elem;
+}
+
+template <class T>
+T& Array<T>::operator [](int index) {
+	return GetAt(index);
+}
+
+template <class T>
+void Array<T>::Add(T elem) {
+	if (lastIndex + 1 == size) {
+		size += grow;
+		initArr(size + grow, size - grow);
+	}
+	arr[lastIndex++ + 1] = elem;
+}
+
+template <class T>
+Array<T>& Array<T>::Append(const Array<T>& obj) {
+	InsertAt(lastIndex + 1, obj.arr, obj.lastIndex + 1);
+	return *this;
+}
+
+template <class T>
+Array<T>& Array<T>::operator=(const Array<T>& obj) {
+	return Append(obj);
+}
+
+template <class T>
+T* Array<T>::GetData() {
+	return arr;
+}
+
+template <class T>
+void Array<T>::InsertAt(int index, T* elems, int size) {
+	if (index < 0 || index >= this->size - 1)
+		return;
+	T* temp = new T[this->size + size];
+
+	int j = 0;
+	for (int i = 0; i < index; i++, j++) {
+		temp[j] = this->arr[i];
+	}
+
+	for (int i = 0; i < size; i++, j++) {
+		temp[j] = elems[i];
+	}
+
+	for (int i = index; i < this->size; i++, j++) {
+		temp[j] = arr[i];
+	}
+	delete[] arr;
+	arr = temp;
+	this->size = this->size + size;
+	lastIndex += size;
+}
+
+template <class T>
+void Array<T>::RemoveAt(int index, int count) {
+	if (index <= 0 || index > size - 1 && count < size) return;
+
+	T* temp = new T[size - count];
+	int j = 0;
+	for (int i = 0; i < index; i++, j++) {
+		temp[j] = arr[i];
+	}
+	for (int i = index + count; i <= lastIndex; i++, j++) {
+		temp[j] = arr[i];
+	}
+	delete[] arr;
+	arr = temp;
+	lastIndex = j - 1;
+}
--- Practic15.05.2024/Practic15.05.2024.cpp
+++ Practic15.05.2024/Practic15.05.2024.cpp
@@ -4,11 +4,49 @@ using namespace std;
 
 int main()
 {
-	Array<int> arr;
-	arr[2] = 8;
-	cout << arr[2];
-	int* num = new int;
-	int* arNum = new int[5] {3, 6, 6};
-	*num = 4;
-	arr.InsertAt(3, arNum);
+	/*Array<int> arr;
+	arr.SetSize(5, 4);
+	cout << arr.GetSize()<< endl;
+
+	cout << arr.GetUpperBound() ;
+	arr.Add(8);
+	arr.Add(47);
+	arr.Add(5);
+	arr.Add(6);
+	cout << arr.GetUpperBound() << endl;
+	cout << arr;
+	arr.Add(1);
+	arr.Add(2);
+	cout << arr.GetSize()<< endl;
+	arr.FreeExtra();
+	cout << arr.GetSize()<< endl;
+	arr.SetAt(4, 60);
+	cout << arr.GetAt(4);
+
+	arr[4] = 78;
+	cout << arr;
+	int* num = new int[10] {1,2,3, 4, 5, 6, 7, 8, 9, 0};
+
+	arr.InsertAt(2, num, 10);
+	cout << arr.GetSize()<< endl;
+	cout << arr;
+	arr.RemoveAt(4, 4);
+	cout << arr;
+	cout << arr.GetData();
+	Array<int> arr1;
+	arr1.SetSize(3);
+	
+	arr1.InsertAt(0, new int[3] {101, 102, 103}, 3);
+	cout << arr1;
+	arr=arr1;
+	cout << arr;*/
+
+	Array<string> arr1;
+	Array<int*> arr2;
+
+	arr1.Add(nullptr);
+	arr1.Add(0);
+	arr1.Add("world");
+
+	cout <<arr1;
 }
